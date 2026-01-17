5e4a42e55403548f64fafbb9b4195f373143adb9
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-01-17 14:40:17 -0300

    keyboardcheck implementation

--- test.pas
+++ test.pas
@@ -1,15 +1,26 @@
 Program testgame ;
 
 uses
-    uVICE;
+    uVICE, crt;
 
 var 
-    result: string;
+    pressed: string;
+    key: char;
+    verification: boolean;
 
 Begin
     pixel(5, 5, 1, 1);	
     pixel(7, 5, 3, 2);		
 
-    result := key('a');		
-    write(result);															
+	{	verification := false;
+
+    repeat 
+        pressed := ReadKey;
+        writeln(pressed);
+    until verification = true;}
+        
+    if keyboardcheck(key) = 'ui_up'	then
+    begin
+        write('up');
+    end;									
 End.
--- uVICE.pas
+++ uVICE.pas
@@ -6,14 +6,45 @@ uses
 	crt;
 
 procedure pixel(x, y, c, t: integer);
-function key(k: char): string;
+function keyboardcheck(k: char): string;
+procedure player(x, y: integer);
 
 implementation
 
-function key(k: char): string;
+function keyboardcheck(k: char): string;
 begin
-	key := 'returning';
+	//repeat
+		k := ReadKey;
+		//key := 'returning';
+		if k = #72 then // up
+		begin
+			keyboardcheck := 'ui_up';
+		end
+		else if k = #80 then // down
+		begin
+			keyboardcheck := 'ui_down';
+		end
+		else if k = #75 then // left
+		begin
+			keyboardcheck := 'ui_left';
+		end
+		else if k = #77 then // right
+		begin
+			keyboardcheck := 'ui_right';
+		end
+		else if k = #27 then // escape
+		begin
+			keyboardcheck := 'ui_escape';
+		end;
+	//until k <> '';
 end; 
+
+procedure player(x, y: integer);
+begin
+	pixel(x, y, 7, 2);
+	pixel(x, y + 1, 7, 2);
+	pixel(x, y + 2, 7, 2);
+end;
 
 procedure pixel(x, y, c, t: integer);
 begin
@@ -28,6 +59,15 @@ begin
 		write('  ');
 	end;
 	textbackground(0);
+end;
+
+procedure update(kind: string);
+begin
+	
+end;
+
+procedure start();
+begin
 end;
 
 end.
