88eb2d5d66e94828dae0fbaea3b549711efff5fc
Daniel (Ray) <thebyt3zcode@yahoo.com>
2026-01-23 15:23:11 -0300

    uses

---
+++ vicelib.pas
@@ -0,0 +1,21 @@
+unit vicelib;
+
+interface
+  uses
+    Windows, Messages;
+
+  procedure pixel();
+  procedure handle();
+
+implementation
+  procedure pixel();
+  begin
+    write('u');
+  end;
+
+  procedure handle();
+  begin
+
+  end;
+
+end.
